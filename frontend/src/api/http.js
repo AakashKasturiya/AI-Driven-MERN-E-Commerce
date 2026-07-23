1426feb8035ec77cf3848538c1d53775e9fdcb8e
aakashkasturiya <aakashkasturiya@gmail.com>
2026-07-23 18:33:27 +0530

    update the http file

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const http = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+  withCredentials: true,
 });
 
 http.interceptors.request.use((config) => {
